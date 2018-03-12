1223430aaf6a9cb3515615072b105de93d8a5ffa
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-03-12 16:06:55 -0500

    Created ProductCategory pivot model

---
+++ Sources/App/Models/ProductCategory.swift
@@ -0,0 +1,25 @@
+import FluentMySQL
+
+final class ProductCategory: MySQLPivot {
+    typealias Left = Product
+    typealias Right = Category
+    
+    static var leftIDKey: WritableKeyPath<ProductCategory, Int> = \.productId
+    static var rightIDKey: WritableKeyPath<ProductCategory, Int> = \.categoryId
+    
+    var productId: Product.ID
+    var categoryId: Category.ID
+    var id: Int?
+    
+    init(product: Product, category: Category)throws {
+        guard let productId = product.id else {
+            fatalError("FIXME: Use a `FluentError`")
+        }
+        guard let categoryId = category.id else {
+            fatalError("FIXME: Use a `FluentError`")
+        }
+        
+        self.productId = productId
+        self.categoryId = categoryId
+    }
+}

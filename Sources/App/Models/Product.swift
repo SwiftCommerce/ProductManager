437dee3c71618421ddca74195a6bac30a33a3a90
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-03-12 17:06:05 -0500

    Created Product.attributes method

--- Sources/App/Models/Product.swift
+++ Sources/App/Models/Product.swift
@@ -3,4 +3,19 @@ final class Product: Content, MySQLModel, Migration {
     let sku: String
     
     init(sku: String) { self.sku = sku }
+    
+    func attributes(with executor: DatabaseConnectable) -> Future<[Attribute]> {
+        let result = Promise<[Attribute]>()
+        
+        if let id = self.id {
+            _ = Attribute.query(on: executor).filter(\.productId == id).all().do { (attributes) in
+                result.complete(attributes)
+            }
+        } else {
+            fatalError("FIXME: Fail prmise with `FluentError`")
+//            result.fail(<#T##error: Error##Error#>)
+        }
+        
+        return result.future
+    }
 }

c53647804c11d236d0b6588a6f74535875403bfd
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-03-13 15:54:17 -0500

    Changed Translation type from generic class to protocol

--- Sources/App/Models/Translation.swift
+++ Sources/App/Models/Translation.swift
@@ -1,18 +1,46 @@
-typealias ProductTranslation = Translation<Product>
-typealias CategoryTranslation = Translation<Category>
+protocol Translation: Content, MySQLModel, Migration {
+    var name: String { get }
+    var description: String { get }
+    var languageCode: String { get }
+    var parentId: Self.ID { get }
+}
 
-final class Translation<Parent: Content & MySQLModel & Migration>: Content, MySQLModel, Migration {
+final class ProductTranslation: Translation {
+    var id: Int?
+
+    let name: String
+    let description: String
+    let languageCode: String
+    let parentId: Int
+    let price: Price
+    
+    init(name: String, description: String, languageCode: String, parentId: Int, price: Price) {
+        self.name = name
+        self.description = description
+        self.languageCode = languageCode
+        self.parentId = parentId
+        self.price = price
+    }
+    
+    convenience init(name: String, description: String, languageCode: String, parentId: Int, price: Float) {
+        let price = Price(price: price, activeFrom: nil, activeTo: nil, active: nil, productId: parentId)
+        self.init(name: name, description: description, languageCode: languageCode, parentId: parentId, price: price)
+    }
+}
+
+final class CategoryTranslation: Translation {
     var id: Int?
     
     let name: String
     let description: String
     let languageCode: String
-    let parentId: Parent.ID
+    let parentId: Int
     
-    init(name: String, description: String, languageCode: String, parentId: Parent.ID) {
+    init(name: String, description: String, languageCode: String, parentId: Int) {
         self.name = name
         self.description = description
         self.languageCode = languageCode
         self.parentId = parentId
     }
 }
+

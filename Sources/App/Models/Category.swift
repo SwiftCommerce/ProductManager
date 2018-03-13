8417afe73a09fc6622b843461f7b9450b9406843
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-03-13 16:02:54 -0500

    Moved CategoryResponseBody type to Category.swift file

--- Sources/App/Models/Category.swift
+++ Sources/App/Models/Category.swift
@@ -38,3 +38,27 @@ final class Category: Content, MySQLModel, Migration {
         category.parentId = id
     }
 }
+
+// MARK: - Public
+
+struct CategoryResponseBody: Content {
+    let id: Int?
+    let name: String
+    let subcategories: [Category]
+}
+
+extension Future where T == CategoryResponseBody {
+    init(category: Category, executedWith executor: DatabaseConnectable) {
+        self = category.subCategories(with: executor).map(to: CategoryResponseBody.self, { (categories) in
+            return CategoryResponseBody(id: category.id, name: category.name, subcategories: categories)
+        })
+    }
+}
+
+extension Future {
+    func category(_ category: Category, with executor: DatabaseConnectable) -> Future<CategoryResponseBody> {
+        return self.flatMap(to: CategoryResponseBody.self, { _ in
+            return Future<CategoryResponseBody>(category: category, executedWith: executor)
+        })
+    }
+}

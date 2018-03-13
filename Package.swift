5d484f89ac8a06793c6e7d404c84098e1c2a91ad
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-03-13 13:17:53 -0500

    Cleaned manifest file

--- Package.swift
+++ Package.swift
@@ -4,11 +4,8 @@ import PackageDescription
 let package = Package(
     name: "ProductManager",
     dependencies: [
-        // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0-rc"),
-
-        // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
-        .package(url: "https://github.com/vapor/fluent-mysql.git", from: "3.0.0-rc"),
+        .package(url: "https://github.com/vapor/fluent-mysql.git", from: "3.0.0-rc")
     ],
     targets: [
         .target(name: "App", dependencies: ["FluentMySQL", "Vapor"]),

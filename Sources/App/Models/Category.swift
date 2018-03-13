e4d74afce46489f8029541b8a92ff786ac4ad46b
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-03-13 07:35:36 -0500

    Created Category.translation method

--- Sources/App/Models/Category.swift
+++ Sources/App/Models/Category.swift
@@ -3,4 +3,21 @@ final class Category: Content, MySQLModel, Migration {
     let name: String
     
     init(name: String) { self.name = name }
+    
+    func translation(with executor: DatabaseConnectable) -> Future<CategoryTranslation> {
+        let result = Promise<CategoryTranslation>()
+        
+        if let id = self.id {
+            _ = CategoryTranslation.query(on: executor).filter(\.parentId == id).first().unwrap(or:
+                    Abort(.internalServerError, reason: "No category translation found for category \(self.id ?? -1)")
+                ).do({ (translation) in
+                    result.complete(translation)
+                })
+        } else {
+            fatalError("FIXME: Fail the future with a `FluentError`")
+//            result.fail(<#T##error: Error##Error#>)
+        }
+        
+        return result.future
+    }
 }

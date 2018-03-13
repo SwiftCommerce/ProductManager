ae0d944b8e7acbe4b3fce14a70dba6cbeaaaeb06
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-03-13 11:37:02 -0500

    Created GET products ProductController route

--- Sources/App/Controllers/ProductController.swift
+++ Sources/App/Controllers/ProductController.swift
@@ -1,3 +1,15 @@
 final class ProductController: RouteCollection {
-    func boot(router: Router) throws {}
+    func boot(router: Router) throws {
+        let products = router.grouped("products")
+        
+        products.get(use: index)
+    }
+    
+    func index(_ request: Request)throws -> Future<[ProductResponseBody]> {
+        return Product.query(on: request).all().flatMap(to: [ProductResponseBody].self, { (products) in
+            return products.map({ (product) in
+                return Future<ProductResponseBody>.init(product: product, executedWith: request)
+            }).flatten()
+        })
+    }
 }

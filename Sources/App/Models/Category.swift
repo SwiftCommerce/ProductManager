3f522eeecd00f05ea6e5fc7aa5ee58c3a40d8967
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-03-13 07:48:45 -0500

    Created Category.assertId method

--- Sources/App/Models/Category.swift
+++ Sources/App/Models/Category.swift
@@ -6,20 +6,22 @@ final class Category: Content, MySQLModel, Migration {
     
     init(name: String) { self.name = name }
     
-    func translation(with executor: DatabaseConnectable) -> Future<CategoryTranslation> {
-        let result = Promise<CategoryTranslation>()
+    func assertId() -> Future<Category.ID> {
+        let result = Promise<Category.ID>()
         
         if let id = self.id {
-            _ = CategoryTranslation.query(on: executor).filter(\.parentId == id).first().unwrap(or:
-                    Abort(.internalServerError, reason: "No category translation found for category \(self.id ?? -1)")
-                ).do({ (translation) in
-                    result.complete(translation)
-                })
+            result.complete(id)
         } else {
-            fatalError("FIXME: Fail the future with a `FluentError`")
+            fatalError("FIXME: Fail promise with `FluentError`")
 //            result.fail(<#T##error: Error##Error#>)
         }
         
         return result.future
+    }
+    
+    func translation(with executor: DatabaseConnectable) -> Future<CategoryTranslation> {
+        return self.assertId().flatMap(to: CategoryTranslation?.self, { (id) in
+            return CategoryTranslation.query(on: executor).filter(\.parentId == id).first()
+        }).unwrap(or: Abort(.internalServerError, reason: "No category translation found for category \(self.id ?? -1)"))
     }
 }

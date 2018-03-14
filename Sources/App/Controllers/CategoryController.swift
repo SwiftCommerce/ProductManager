c75421681234d843f6459227b109162fefda4495
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-03-14 08:08:27 -0500

    Created GET /categories CategoryController route

--- Sources/App/Controllers/CategoryController.swift
+++ Sources/App/Controllers/CategoryController.swift
@@ -1,3 +1,13 @@
 final class CategoryController: RouteCollection {
-    func boot(router: Router) throws {}
+    func boot(router: Router) throws {
+        let categories = router.grouped("categories")
+        
+        categories.get(use: index)
+    }
+    
+    func index(_ request: Request)throws -> Future<[CategoryResponseBody]> {
+        return Category.query(on: request).all().flatMap(to: [CategoryResponseBody].self, { (categories) in
+            categories.map({ Future(category: $0, executedWith: request) }).flatten()
+        })
+    }
 }

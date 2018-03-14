7bbbf6c61cf4eb5353d6256bf867d62c658d2f67
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-03-14 08:19:47 -0500

    Created DELETE /categories/{Category} CategoryController route

--- Sources/App/Controllers/CategoryController.swift
+++ Sources/App/Controllers/CategoryController.swift
@@ -6,6 +6,8 @@ final class CategoryController: RouteCollection {
         categories.get(Category.parameter, use: show)
         
         categories.post(use: create)
+        
+        categories.delete(Category.parameter, use: delete)
     }
     
     func index(_ request: Request)throws -> Future<[CategoryResponseBody]> {
@@ -21,5 +23,9 @@ final class CategoryController: RouteCollection {
     func create(_ request: Request)throws -> Future<CategoryResponseBody> {
         let name = request.content.get(String.self, at: "name")
         return name.map(to: Category.self, { Category(name: $0) }).save(on: request).response(with: request)
+    }
+    
+    func delete(_ request: Request)throws -> Future<HTTPStatus> {
+        return try request.parameter(Category.self).delete(on: request).transform(to: .noContent)
     }
 }

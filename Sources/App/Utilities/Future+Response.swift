bc35c499eb8941f840440a7bc32639d1cffe5a4b
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-03-16 14:35:32 -0500

    Created Future<Collection>.loop methods

--- Sources/App/Utilities/Future+Response.swift
+++ Sources/App/Utilities/Future+Response.swift
@@ -22,3 +22,17 @@ extension Future where T: Translation {
         })
     }
 }
+
+extension Future where T: Collection {
+    func loop<R>(to: R.Type, transform: @escaping (T.Element)throws -> R) -> Future<[R]> {
+        return self.map(to: [R].self, { (sequence) in
+            return try sequence.map(transform)
+        })
+    }
+    
+    func loop<R>(to: R.Type, transform: @escaping (T.Element)throws -> Future<R>) -> Future<[R]> {
+        return self.flatMap(to: [R].self, { (sequence) in
+            return try sequence.map(transform).flatten()
+        })
+    }
+}

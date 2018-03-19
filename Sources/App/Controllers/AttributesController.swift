d857c3c2086146f27ea5c2ff55070d38e9f09162
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-03-19 08:36:53 -0500

    Created POST /products/{Product}/attributes AttributesController route

--- Sources/App/Controllers/AttributesController.swift
+++ Sources/App/Controllers/AttributesController.swift
@@ -3,9 +3,15 @@ final class AttributesController: RouteCollection {
         let attributes = router.grouped("products", Product.parameter, "attributes")
         
         attributes.get(use: index)
+        
+        attributes.post(Attribute.self, use: create)
     }
     
     func index(_ request: Request)throws -> Future<[Attribute]> {
         return try request.parameter(Product.self).flatMap(to: [Attribute].self, { $0.attributes(with: request) })
+    }
+    
+    func create(_ request: Request, _ attribute: Attribute)throws -> Future<Attribute> {
+        return attribute.save(on: request)
     }
 }

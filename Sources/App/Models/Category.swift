aefdcd6b1e36e044be6366d12e00bd3bdc1b8beb
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-03-19 15:21:59 -0500

    Removed Category relation methods and properties from Category model in favor of pivot

--- Sources/App/Models/Category.swift
+++ Sources/App/Models/Category.swift
@@ -2,7 +2,6 @@ final class Category: Content, MySQLModel, Migration, Parameter {
     var id: Int?
     
     let name: String
-    var parentId: Category.ID?
     
     init(name: String) { self.name = name }
     
@@ -10,19 +9,6 @@ final class Category: Content, MySQLModel, Migration, Parameter {
         return self.assertID().flatMap(to: [CategoryTranslation].self, { (id) in
             return try self.translations.query(on: executor).all()
         })
-    }
-    
-    func subCategories(with executor: DatabaseConnectable) -> Future<[Category]> {
-        return self.assertID().flatMap(to: [Category].self, { (id) in
-            return Category.query(on: executor).filter(\.parentId == id).all()
-        })
-    }
-    
-    func add(child category: Category)throws {
-        guard let id = self.id else {
-            fatalError("FIXME: Throw a `FluentError`")
-        }
-        category.parentId = id
     }
 }
 
@@ -37,17 +23,19 @@ struct CategoryResponseBody: Content {
 
 extension Future where T == CategoryResponseBody {
     init(category: Category, executedWith executor: DatabaseConnectable) {
-        let categories = category.subCategories(with: executor).flatMap(to: [CategoryResponseBody].self) { (categories) in
-            return categories.map({ Future(category: $0, executedWith: executor) }).flatten()
-        }
-        
-        self = Async.map(to: CategoryResponseBody.self, categories, category.translations(with: executor)) { (subCategories, translations) in
-            return CategoryResponseBody(
-                id: category.id,
-                name: category.name,
-                subcategories: subCategories,
-                translations: translations.map({ TranslationResponseBody($0, price: nil) })
-            )
-        }
+        self = Future.flatMap({
+            let categories = try category.subCategories.query(on: executor).all().flatMap(to: [CategoryResponseBody].self) { (categories) in
+                return categories.map({ Future(category: $0, executedWith: executor) }).flatten()
+            }
+            
+            return Async.map(to: CategoryResponseBody.self, categories, category.translations(with: executor)) { (subCategories, translations) in
+                return CategoryResponseBody(
+                    id: category.id,
+                    name: category.name,
+                    subcategories: subCategories,
+                    translations: translations.map({ TranslationResponseBody($0, price: nil) })
+                )
+            }
+        })
     }
 }

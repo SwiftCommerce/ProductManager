bd85caf9fd836b13c79ea234f129aa0af2eabc73
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-03-19 12:06:32 -0500

    Created PATCH /products/{Product}/attributes/{Int} AttributesController route

--- Sources/App/Controllers/AttributesController.swift
+++ Sources/App/Controllers/AttributesController.swift
@@ -4,12 +4,23 @@ final class AttributesController: RouteCollection {
     func boot(router: Router) throws {
         let attributes = router.grouped("products", Product.parameter, "attributes")
         
+        attributes.post(Attribute.self, use: create)
+        
         attributes.get(use: index)
         attributes.get(Int.parameter, use: show)
         
-        attributes.post(Attribute.self, use: create)
+        attributes.patch(Int.parameter, use: update)
         
         attributes.delete(Attribute.parameter, use: delete)
+    }
+    
+    func create(_ request: Request, _ attribute: Attribute)throws -> Future<Attribute> {
+        return Attribute.query(on: request).filter(\.name == attribute.name).count().flatMap(to: Attribute.self) { (attributeCount) in
+            guard attributeCount < 1 else {
+                throw Abort(.badRequest, reason: "Attribute already exists for product with name '\(attribute.name)'")
+            }
+            return attribute.save(on: request)
+        }
     }
     
     func index(_ request: Request)throws -> Future<[Attribute]> {
@@ -23,13 +34,14 @@ final class AttributesController: RouteCollection {
         }
     }
     
-    func create(_ request: Request, _ attribute: Attribute)throws -> Future<Attribute> {
-        return Attribute.query(on: request).filter(\.name == attribute.name).count().flatMap(to: Attribute.self) { (attributeCount) in
-            guard attributeCount < 1 else {
-                throw Abort(.badRequest, reason: "Attribute already exists for product with name '\(attribute.name)'")
-            }
-            return attribute.save(on: request)
-        }
+    func update(_ request: Request)throws -> Future<Attribute> {
+        let product = try request.parameter(Product.self)
+        let newValue = request.content.get(String.self, at: "value")
+        
+        return flatMap(to: Attribute.self, product, newValue, { (product, newValue) in
+            let id = try request.parameter(Int.self)
+            return try product.attributes.query(on: request).filter(\.id == id).set(\.value, to: newValue)
+        })
     }
     
     func delete(_ request: Request)throws -> Future<HTTPStatus> {

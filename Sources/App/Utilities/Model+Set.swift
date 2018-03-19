55e55db2e43941d45e5eed8905018eb6a468a68f
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-03-19 12:05:03 -0500

    Created QueryBuilder.set methods for returning Model, [Model], and Void

--- Sources/App/Utilities/Model+Set.swift
+++ Sources/App/Utilities/Model+Set.swift
@@ -1,6 +1,22 @@
 import CodableKit
 
 extension QueryBuilder {
+    func set<Value>(_ key: KeyPath<Model, Value>, to value: Value) -> Future<Model> where Value: Encodable & KeyStringDecodable {
+        self.query.data = [
+            key.makeQueryField().name: value
+        ]
+        self.query.action = .update
+        return self.first().unwrap(or: Abort(.notFound, reason: "No \(Model.entity) model found with given query"))
+    }
+    
+    func set<Value>(_ key: KeyPath<Model, Value>, to value: Value) -> Future<[Model]> where Value: Encodable & KeyStringDecodable {
+        self.query.data = [
+            key.makeQueryField().name: value
+        ]
+        self.query.action = .update
+        return self.all()
+    }
+    
     func set<Value>(_ key: KeyPath<Model, Value>, to value: Value) -> Future<Void> where Value: Encodable & KeyStringDecodable {
         self.query.data = [
             key.makeQueryField().name: value

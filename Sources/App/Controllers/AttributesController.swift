ffaa152acd4820e060335fbc1f33b9ed572ae4f2
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-03-19 10:46:09 -0500

    Created GET /products/{Product}/attributes/{Int} AttributesController route

--- Sources/App/Controllers/AttributesController.swift
+++ Sources/App/Controllers/AttributesController.swift
@@ -1,14 +1,24 @@
+import Async
+
 final class AttributesController: RouteCollection {
     func boot(router: Router) throws {
         let attributes = router.grouped("products", Product.parameter, "attributes")
         
         attributes.get(use: index)
+        attributes.get(Int.parameter, use: show)
         
         attributes.post(Attribute.self, use: create)
     }
     
     func index(_ request: Request)throws -> Future<[Attribute]> {
         return try request.parameter(Product.self).flatMap(to: [Attribute].self, { try $0.attributes.query(on: request).all() })
+    }
+    
+    func show(_ request: Request)throws -> Future<Attribute> {
+        return try request.parameter(Product.self).flatMap(to: Attribute.self) { (product) in
+            let id = try request.parameter(Int.self)
+            return try product.attributes.query(on: request).filter(\.id == id).first().unwrap(or: Abort(.notFound, reason: "No attribute connected to product with ID '\(id)'"))
+        }
     }
     
     func create(_ request: Request, _ attribute: Attribute)throws -> Future<Attribute> {

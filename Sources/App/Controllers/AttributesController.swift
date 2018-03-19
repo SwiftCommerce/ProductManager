4630885a2addecd09af03e73a5a9c1fefe8713c5
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-03-19 08:33:15 -0500

    Created GET /products/{Product}/attributes AttributesController route

--- Sources/App/Controllers/AttributesController.swift
+++ Sources/App/Controllers/AttributesController.swift
@@ -1,3 +1,11 @@
 final class AttributesController: RouteCollection {
-    func boot(router: Router) throws {}
+    func boot(router: Router) throws {
+        let attributes = router.grouped("products", Product.parameter, "attributes")
+        
+        attributes.get(use: index)
+    }
+    
+    func index(_ request: Request)throws -> Future<[Attribute]> {
+        return try request.parameter(Product.self).flatMap(to: [Attribute].self, { $0.attributes(with: request) })
+    }
 }

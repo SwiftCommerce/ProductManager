53f16b6f84de38eeb61c1c0c5e0ce728c5514967
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-03-19 10:03:03 -0500

    Created ProductAttribute pivot model

---
+++ Sources/App/Models/ProductAttribute.swift
@@ -0,0 +1,36 @@
+final class ProductAttribute: MySQLPivot {
+    typealias Left = Product
+    typealias Right = Attribute
+    
+    static var leftIDKey: WritableKeyPath<ProductAttribute, Int> = \.productId
+    static var rightIDKey: WritableKeyPath<ProductAttribute, Int> = \.attributeId
+    
+    var id: Int?
+    
+    var productId: Int
+    var attributeId: Int
+    
+    init(product: Product, attribute: Attribute)throws {
+        guard let productId = product.id else {
+            fatalError("FIXME: Use a `FluentError`")
+        }
+        guard let attributeId = attribute.id else {
+            fatalError("FIXME: Use a `FluentError`")
+        }
+        
+        self.productId = productId
+        self.attributeId = attributeId
+    }
+}
+
+extension Product {
+    var attributes: Siblings<Product, Attribute, ProductAttribute> {
+        return self.siblings()
+    }
+}
+
+extension Attribute {
+    var products: Siblings<Attribute, Product, ProductAttribute> {
+        return self.siblings()
+    }
+}

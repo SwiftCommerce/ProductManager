29efd8b45660c1ded4a20ca6acdfcbcfbcc16919
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-03-19 15:24:06 -0500

    Replaced do/catch in Model.assertID with static Future.map method

--- Sources/App/Utilities/Model+ID.swift
+++ Sources/App/Utilities/Model+ID.swift
@@ -1,9 +1,7 @@
 extension Model {
     func assertID() -> Future<Self.ID> {
-        do {
-            return try Future(self.requireID())
-        } catch {
-            return Future(error: error)
-        }
+        return Future.map({
+            return try self.requireID()
+        })
     }
 }

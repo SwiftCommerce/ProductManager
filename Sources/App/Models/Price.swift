ec1125539747f823172f75d257fd2f8b7409b79b
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-03-20 15:51:33 -0500

    Documented Price and PriceUpdateBody types

--- Sources/App/Models/Price.swift
+++ Sources/App/Models/Price.swift
@@ -1,14 +1,42 @@
 import Foundation
 
+/// The price for a product.
+/// A price is connected to a translations because you need to
+/// use different currencies for a single price.
+/// You also might want to do a region specific discount.
 final class Price: Content, MySQLModel, Migration {
+    
+    /// The database ID of the model.
     var id: Int?
     
+    /// The amount of the translation's currency required for the product's purchase.
     var price: Float
+    
+    /// The date the price became valid on.
     var activeFrom: Date
+    
+    /// The date the price is no longer valid.
     var activeTo: Date
+    
+    /// Wheather or not the price is the current price of the product.
     var active: Bool
+    
+    /// The name of the trsnlation that owns the model.
+    /// This allows easy querying on the trnslations side to get related prices:
+    ///
+    ///     SELECT ... WHERE trnslationName == name
     let translationName: ProductTranslation.ID
     
+    
+    /// Creates a new `Price` model from given data.
+    /// Make sure you call `.save` on it to store it in the database.
+    ///
+    /// - Parameters:
+    ///   - price: The amount of the owning translation's current is needed to purchase the given product.
+    ///   - activeFrom: The date the price starts being valid. If you pass in `nil`, it defaults to the time the price is created (`Date()`).
+    ///   - activeTo: The date the price becomes invalid. If you pass in `nil`, it defaults to some time in the distant future (`Date.distantFuture`).
+    ///   - active: Wheather or not the price is valid. If you pass in `nil`, the value is calculated of the `activeFrom` and `activeTo` dates.
+    ///   - translationName: The name of the translation that owns the price.
     init(price: Float, activeFrom: Date?, activeTo: Date?, active: Bool?, translationName: ProductTranslation.ID) {
         let af = activeFrom ?? Date()
         let at: Date = activeTo ?? Date.distantFuture
@@ -20,6 +48,7 @@ final class Price: Content, MySQLModel, Migration {
         self.translationName = translationName
     }
     
+    // We have a custom decoding init so we can have the same default values as the ones in the main init.
     convenience init(from decoder: Decoder)throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         try self.init(
@@ -30,21 +59,43 @@ final class Price: Content, MySQLModel, Migration {
             translationName: container.decode(ProductTranslation.ID.self, forKey: .translationName)
         )
         
+        // This init mwthod is used by Fluent to initailize an instance of the class,
+        // so we need to assign all properties.
+        // This method is also used by Fluent to create the tables in the datbase.
         self.id = try container.decodeIfPresent(Int.self, forKey: .id)
     }
     
+    
+    /// Updates the model with data from a request and saves it.
+    ///
+    /// - Parameters:
+    ///   - body: The body of a request, decoded to a `PriceUpdateBody`.
+    ///   - executor: The object that will be used to save the model to the database.
+    /// - Returns: A void future, which will signal once the update is complete.
     func update(with body: PriceUpdateBody, on executor: DatabaseConnectable) -> Future<Void> {
+        // Update all the properties if a value for it is found in the body, else use the old value.
         self.price = body.price ?? self.price
         self.activeFrom = body.activeFrom ?? self.activeFrom
         self.activeTo = body.activeTo ?? self.activeTo
         self.active = body.active ?? self.active
+        
+        // Update the model in the database.
         return self.save(on: executor).transform(to: ())
     }
 }
 
+/// A representation of a request's body when you need to update a `Price` model.
 struct PriceUpdateBody: Content {
+    
+    ///
     let price: Float?
+    
+    ///
     let activeFrom: Date?
+    
+    ///
     let activeTo: Date?
+    
+    ///
     let active: Bool?
 }

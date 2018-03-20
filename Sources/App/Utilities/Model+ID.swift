f69a3ff6f8ec835316461113ecb9811c4bb07dc3
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-03-20 12:23:56 -0500

    Documented Model.assertID method

--- Sources/App/Utilities/Model+ID.swift
+++ Sources/App/Utilities/Model+ID.swift
@@ -1,6 +1,14 @@
 extension Model {
+    
+    /// Gets the model's ID as a future so you can chain it with `.map` and it doesn't throw.
+    ///
+    /// - Returns: The model's ID wrapped in a future.
     func assertID() -> Future<Self.ID> {
+        // Create a new future with the static `Future.map` mthod.
         return Future.map({
+            
+            // Get the model's ID or throw an error.
+            // The error is caught by the future and causes it to fail.
             return try self.requireID()
         })
     }

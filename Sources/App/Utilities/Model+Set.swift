561f4f9977afb89b721125b95f457b97df6ae130
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-03-20 11:13:47 -0500

    Removed un-used QueryBuilder.set methods

--- Sources/App/Utilities/Model+Set.swift
+++ Sources/App/Utilities/Model+Set.swift
@@ -8,23 +8,7 @@ extension QueryBuilder {
         self.query.action = .update
         return self.first().unwrap(or: Abort(.notFound, reason: "No \(Model.entity) model found with given query"))
     }
-    
-    func set<Value>(_ key: KeyPath<Model, Value>, to value: Value) -> Future<[Model]> where Value: Encodable & KeyStringDecodable {
-        self.query.data = [
-            key.makeQueryField().name: value
-        ]
-        self.query.action = .update
-        return self.all()
-    }
-    
-    func set<Value>(_ key: KeyPath<Model, Value>, to value: Value) -> Future<Void> where Value: Encodable & KeyStringDecodable {
-        self.query.data = [
-            key.makeQueryField().name: value
-        ]
-        self.query.action = .update
-        return self.execute()
-    }
-    
+        
     @discardableResult
     public func filter<T>(_ field: KeyPath<Model, T>, in values: [Encodable]?) -> Future<[Model]> where T: KeyStringDecodable {
         guard let values = values else {

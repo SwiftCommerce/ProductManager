eb04e0fd726cf9111eb6bbd4a78ac69a18613b41
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-03-20 14:37:24 -0500

    Documented Attribute model

--- Sources/App/Models/Attribute.swift
+++ Sources/App/Models/Attribute.swift
@@ -1,9 +1,16 @@
+/// A custom, user-defined attribute for a product
 final class Attribute: Content, MySQLModel, Migration, Parameter {
+    
+    /// The database ID of the model.
     var id: Int?
     
+    /// The name of the attribute, given by the user.
     let name: String
+    
+    /// The value of the attribute, given by the user.
     var value: String
     
+    ///
     init(name: String, value: String) {
         self.name = name
         self.value = value

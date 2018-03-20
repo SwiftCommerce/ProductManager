b9de64db66f6ba91117f5322aab5ee2d59bf3541
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-03-20 11:28:15 -0500

    Documented Future<Model>.response methods

--- Sources/App/Utilities/Future+Response.swift
+++ Sources/App/Utilities/Future+Response.swift
@@ -1,4 +1,13 @@
+// The methods below convert a model to a struct the represents the model.
+// They take in an object that conforms to `DatabaseConnectable` so they can get related models.
+
+/// Exend `Future` if it wraps a `Category` model.
 extension Future where T == Category {
+    
+    /// Create a publicizable represention of the wrapped model, appropriate for returning as JSON.
+    ///
+    /// - Parameter executor: An object that can be used to query a model's table.
+    /// - Returns: A future wrapping the public representation of the wrapped object.
     func response(with executor: DatabaseConnectable) -> Future<CategoryResponseBody> {
         return self.flatMap(to: CategoryResponseBody.self, { this in
             return Future<CategoryResponseBody>(category: this, executedWith: executor)
@@ -6,8 +15,13 @@ extension Future where T == Category {
     }
 }
 
-
+/// Extend `Future` if it wraps a `Product` model.
 extension Future where T == Product {
+    
+    /// Create a publicizable represention of the wrapped model, appropriate for returning as JSON.
+    ///
+    /// - Parameter executor: An object that can be used to query a model's table.
+    /// - Returns: A future wrapping the public representation of the wrapped object.
     func response(with executor: DatabaseConnectable) -> Future<ProductResponseBody> {
         return self.flatMap(to: ProductResponseBody.self, { this in
             return Future<ProductResponseBody>(product: this, executedWith: executor)
@@ -15,7 +29,13 @@ extension Future where T == Product {
     }
 }
 
+/// Extend `Future` if it wraps a model conforming to `Trnslation`.
 extension Future where T: Translation {
+    
+    /// Create a publicizable represention of the wrapped model, appropriate for returning as JSON.
+    ///
+    /// - Parameter executor: An object that can be used to query a model's table.
+    /// - Returns: A future wrapping the public representation of the wrapped object.
     func response(on executor: DatabaseConnectable) -> Future<TranslationResponseBody> {
         return self.flatMap(to: TranslationResponseBody.self, { (this) in
             return this.response(on: executor)

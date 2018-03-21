85e3438dea6274d78e35e11a3689933bb5ec7363
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-03-21 09:34:24 -0500

    Documented ProductCategory pivot model

--- Sources/App/Models/ProductCategory.swift
+++ Sources/App/Models/ProductCategory.swift
@@ -1,5 +1,4 @@
-import FluentMySQL
-
+// A pivot model for connecting `Category` models to a `Product` model.
 final class ProductCategory: MySQLPivot, Migration {
     typealias Left = Product
     typealias Right = Category
@@ -11,10 +10,15 @@ final class ProductCategory: MySQLPivot, Migration {
     var categoryId: Category.ID
     var id: Int?
     
+    /// Create a pivot from a `Product` and `Attribute` model.
     init(product: Product, category: Category)throws {
+        
+        // Verfiy the `product` has been save to the database (that it has an ID).
         guard let productId = product.id else {
             fatalError("FIXME: Use a `FluentError`")
         }
+        
+        // Verfiy the `category` has been save to the database.
         guard let categoryId = category.id else {
             fatalError("FIXME: Use a `FluentError`")
         }
@@ -24,9 +28,16 @@ final class ProductCategory: MySQLPivot, Migration {
     }
 }
 
+/// Extend `Siblings` model if the `Base` model's `Database` type conforms to `QuerySupporting` and the `Base` model's `ID` type conforms to `KeyStringDecodable`.
 extension Siblings where Base.Database: QuerySupporting, Base.ID: KeyStringDecodable {
+    
+    /// Delets all pivot rows connecting `Base` model to any `Related` models.
     func deleteConnections(on executor: DatabaseConnectable) -> Future<Void> {
+        
+        // Wrap the query in a `flatMap` so the `deleteConnections` method doesn't throw.
         return Future.flatMap {
+            
+            // Run `DELETE` query on all pivot rows that have the `Base` model's ID.
             return try Through.query(on: executor).filter(self.basePivotField == self.base.requireID()).delete()
         }
     }

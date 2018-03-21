dfc79bd41510ac88baa7148f65b91e659b3ff4e4
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-03-21 09:11:22 -0500

    Document ProductAttribute model

--- Sources/App/Models/ProductAttribute.swift
+++ Sources/App/Models/ProductAttribute.swift
@@ -1,3 +1,4 @@
+/// A pivot model for connecting `Attribute` models to a `Product` model.
 final class ProductAttribute: MySQLPivot, Migration {
     typealias Left = Product
     typealias Right = Attribute
@@ -10,10 +11,15 @@ final class ProductAttribute: MySQLPivot, Migration {
     var productId: Int
     var attributeId: Int
     
+    /// Create a pivot from a `Product` and `Attribute` model.
     init(product: Product, attribute: Attribute)throws {
+        
+        // Verfiy the `product` has been save to the database (that it has an ID).
         guard let productId = product.id else {
             fatalError("FIXME: Use a `FluentError`")
         }
+        
+        // Verify the `attribute` has been saved to the database.
         guard let attributeId = attribute.id else {
             fatalError("FIXME: Use a `FluentError`")
         }

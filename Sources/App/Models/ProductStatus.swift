0027f164587dfaa3ed7f78450d044ee2a02c3b7d
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-03-23 10:40:41 -0500

    Created ProdcutStatus enum

--- Sources/App/Models/ProductStatus.swift
+++ Sources/App/Models/ProductStatus.swift
@@ -1,4 +1,25 @@
-//enum ProductStatus: Int, Codable, KeyStringDecodable, MySQLDataConvertible {
-//    case draft
-//}
+enum ProductStatus: Int, Codable, KeyStringDecodable, MySQLDataConvertible {
+    case draft
+    case published
+    case deactivated
+    
+    static var keyStringTrue: ProductStatus = .published
+    static var keyStringFalse: ProductStatus = .deactivated
+    
+    func convertToMySQLData() throws -> MySQLData {
+        return .init(integer: self.rawValue)
+    }
+    
+    static func convertFromMySQLData(_ mysqlData: MySQLData) throws -> ProductStatus {
+        guard let rawValue = try mysqlData.integer(Int.self) else {
+            throw MySQLError(identifier: "badType", reason: "A `ProdcutStatus` case must be initialized with an integer", source: .capture())
+        }
+        
+        guard let status = ProductStatus(rawValue: rawValue) else {
+            throw MySQLError(identifier: "badInt", reason: "No case exists for`ProductStatus` enum with the raw value \(rawValue)", source: .capture())
+        }
+        
+        return status
+    }
+}
 

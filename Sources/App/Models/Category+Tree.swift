1f082d42e9cb86d98d3cdfb482de8e39e0c03e9f
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-03-23 11:38:02 -0500

    Created Category.tree method

--- Sources/App/Models/Category+Tree.swift
+++ Sources/App/Models/Category+Tree.swift
@@ -9,3 +9,30 @@ struct CategoryNode {
     /// the main category through `CategoryPivot` models.
     let children: [CategoryNode]
 }
+
+extension Category {
+    
+    /// Gets the category's sub-categories, and their sub-categories, and so on.
+    ///
+    /// - Parameter request: The request that will be used to both run the database queries and create new promises.
+    /// - Returns: A tree structure of categories, made up of nodes.
+    func tree(with request: Request) -> Future<CategoryNode> {
+        
+        // Wrap the methods implementation in a `.flatMap` clsure, so the method doesn't have to throw.
+        return Future.flatMap(on: request, { () -> EventLoopFuture<CategoryNode> in
+            
+            // Get all of the category's sub-categories.
+            let children = try! self.subCategories.query(on: request).all()
+            
+            return children.flatMap(to: [CategoryNode].self) { (children) in
+                
+                // Get all the sub-categories' nodes.
+                return children.map({ $0.tree(with: request) }).flatten(on: request)
+            }.map(to: CategoryNode.self) { (childNodes) in
+                
+                // Create the top node from the current categories and child nodes.
+                return CategoryNode(category: self, children: childNodes)
+            }
+        })
+    }
+}

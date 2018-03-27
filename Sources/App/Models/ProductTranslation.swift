6926257dc3d32e0821dfbaa54f3f6d072db80b27
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-03-27 08:33:54 -0500

    Replaced ModelTranslation pivot and .sibling calls with TranslationParent.translations(on:) request

--- Sources/App/Models/ProductTranslation.swift
+++ Sources/App/Models/ProductTranslation.swift
@@ -1,50 +1,70 @@
-/// A static type declaration of `ModelTranslation` with `Parent` and `ParentTransltion` models.
-typealias ProductTranslationPivot = ModelTranslation<Product, ProductTranslation>
-
-/// A static type declaration of `ModelTranslation` with `Category` and `CategoryTransltion` models.
-typealias CategoryTranslationPivot = ModelTranslation<Category, CategoryTranslation>
+///// A static type declaration of `ModelTranslation` with `Parent` and `ParentTransltion` models.
+//typealias ProductTranslationPivot = ModelTranslation<Product, ProductTranslation>
+//
+///// A static type declaration of `ModelTranslation` with `Category` and `CategoryTransltion` models.
+//typealias CategoryTranslationPivot = ModelTranslation<Category, CategoryTranslation>
 
 /// Defines a model as being connected to `Translation` models through a `ModelTranslation` pivot.
 protocol TranslationParent: MySQLModel {
     
     /// The translation model type that the parent model connects to.
-    associatedtype TranslationType: Translation
+    associatedtype Translation: App.Translation
     
-    /// A property to access connected translations through a `ModelTranslation` pivot.
-    var translations: Siblings<Self, TranslationType, ModelTranslation<Self, TranslationType>> { get }
+    /// Creates a query to access connected translations by their `parentID` property.
+    ///
+    /// - parameter executor: The object that gets the connetion to the database,
+    ///   which is used to run the query.
+    func translations(on executor: DatabaseConnectable) -> QueryBuilder<Translation, Translation>
 }
 
-/// A generic pivot type for connecting a model to a `Translation` model.
-final class ModelTranslation<Parent: MySQLModel, Translation: App.Translation>: MySQLPivot, Migration {
-    typealias Left = Parent
-    typealias Right = Translation
+extension TranslationParent {
     
-    static var leftIDKey: WritableKeyPath<ModelTranslation, Int> {
-        return \.parentId
-    }
-    
-    static var rightIDKey: WritableKeyPath<ModelTranslation, String> {
-        return \.translationName
-    }
-    
-    var parentId: Parent.ID
-    var translationName: Translation.ID
-    var id: Int?
-    
-    /// Create a pivot from the defined `Parent` and `Trans` types.
-    init(parent: Parent, translation: Translation)throws {
+    /// Creates a query to access connected translations by their `parentID` property.
+    ///
+    /// - parameter executor: The object that gets the connetion to the database,
+    ///   which is used to run the query.
+    func translations(on executor: DatabaseConnectable)throws -> QueryBuilder<Translation, Translation> {
         
-        // Verify the `parent` model has been saved to the database (by checking for the ID).
-        guard let parentId = parent.id else {
-            fatalError("FIXME: Use a `FluentError`")
-        }
+        // The parent model has to be saved to the daatbase to be connected to any translations,
+        // so make sure it exists in the DB.
+        let id = try self.requireID()
         
-        // Verify the `translation` model has been saved to the database.
-        guard let translationName = translation.name else {
-            fatalError("FIXME: Use a `FluentError`")
-        }
-        
-        self.parentId = parentId
-        self.translationName = translationName
+        // Get all the translation models twho's `parentID` is the ID of the current model.
+        return try Translation.query(on: executor).filter(\.parentID == id)
     }
 }
+
+///// A generic pivot type for connecting a model to a `Translation` model.
+//final class ModelTranslation<Parent: MySQLModel, Translation: App.Translation>: MySQLPivot, Migration {
+//    typealias Left = Parent
+//    typealias Right = Translation
+//
+//    static var leftIDKey: WritableKeyPath<ModelTranslation, Int> {
+//        return \.parentId
+//    }
+//
+//    static var rightIDKey: WritableKeyPath<ModelTranslation, String> {
+//        return \.translationName
+//    }
+//
+//    var parentId: Parent.ID
+//    var translationName: Translation.ID
+//    var id: Int?
+//
+//    /// Create a pivot from the defined `Parent` and `Trans` types.
+//    init(parent: Parent, translation: Translation)throws {
+//
+//        // Verify the `parent` model has been saved to the database (by checking for the ID).
+//        guard let parentId = parent.id else {
+//            fatalError("FIXME: Use a `FluentError`")
+//        }
+//
+//        // Verify the `translation` model has been saved to the database.
+//        guard let translationName = translation.name else {
+//            fatalError("FIXME: Use a `FluentError`")
+//        }
+//
+//        self.parentId = parentId
+//        self.translationName = translationName
+//    }
+//}
--- Sources/App/Utilities/Siblings+ModelPivot.swift
+++ Sources/App/Utilities/Siblings+ModelPivot.swift
@@ -33,38 +33,3 @@ extension Category {
         return self.siblings(related: Category.self, through: CategoryPivot.self, CategoryPivot.rightIDKey, CategoryPivot.leftIDKey)
     }
 }
-
-
-// MARK: - Translation Pivots
-
-extension Product: TranslationParent {
-    
-    /// Gets the translations connected to the current `Product` model.
-    var translations: Siblings<Product, ProductTranslation, ProductTranslationPivot> {
-        return siblings()
-    }
-}
-
-extension Category: TranslationParent {
-    
-    /// Gets the translations connected to the current `Category` model.
-    var translations: Siblings<Category, CategoryTranslation, CategoryTranslationPivot> {
-        return siblings()
-    }
-}
-
-extension ProductTranslation {
-    
-    /// Gets the products connected to the current `ProductTranslation` model.
-    var products: Siblings<ProductTranslation, Product, ProductTranslationPivot> {
-        return siblings()
-    }
-}
-
-extension CategoryTranslation {
-    
-    /// Gets the categories connected to the current `CategoryTranslation` model.
-    var categories: Siblings<CategoryTranslation, Category, CategoryTranslationPivot> {
-        return siblings()
-    }
-}

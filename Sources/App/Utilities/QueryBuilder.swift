59251e4594d3cfed7efd8c280bf70481d401373c
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-04-02 10:41:39 -0500

    Cleaned QueryBuilder.models(where:in:) implementation

--- Sources/App/Utilities/QueryBuilder.swift
+++ Sources/App/Utilities/QueryBuilder.swift
@@ -10,20 +10,16 @@ extension QueryBuilder {
     /// - Returns: All the models that match the given query, wrapped in a future.
     @discardableResult
     public func models<Value>(where field: KeyPath<Model, Value>, in values: [Value]?) -> Future<[Result]> where Value: ReflectionDecodable {
+        
         // This method is different because we allow `nil` to be passed in instead of an array.
         // If we get `nil` instead of an array, return an empty array immediately, it saves time.
         guard let values = values else {
-            let emptyResult = self.connection.eventLoop.newPromise([Result].self)
-            emptyResult.succeed(result: [])
-            return emptyResult.futureResult
+            return self.connection.eventLoop.newSucceededFuture(result: [])
         }
         
         // Wrap filter in `flatMap` so the method doesn't have to throw.
-        return Future.flatMap(on: self.connection.eventLoop) { () -> EventLoopFuture<[Result]> in
-            
-            // Since `value` is not `nil`, run the filter and get all the resulting models.
-            return try self.filter(field ~~ values).all()
-        }
+        // Since `value` is not `nil`, run the filter and get all the resulting models.
+        return Future.flatMap(on: self.connection.eventLoop) { return try self.filter(field ~~ values).all() }
     }
 }
 

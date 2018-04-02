d526e7e3f1cf9c9d2df383987810ca23fac166e3
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-04-02 10:48:37 -0500

    Verify left and right categories in CategoryPivot are not the same

--- Sources/App/Models/CategoryPivot.swift
+++ Sources/App/Models/CategoryPivot.swift
@@ -20,6 +20,16 @@ final class CategoryPivot: MySQLPivot, Migration {
         // Verify the right `category` model has been saved to the database.
         let right = try rightCategory.requireID()
         
+        // Verfiy we aren't trying to create a pivot
+        // with the same model on the both sides if the pivot.
+        guard left != right else {
+            throw FluentError(
+                identifier: "identicalCategoryIDs",
+                reason: "Can't create a `CategoryPivot` instance with with the same model for both the left and right categories.",
+                source: .capture()
+            )
+        }
+        
         self.right = right
         self.left = left
     }

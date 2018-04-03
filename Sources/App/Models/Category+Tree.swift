966708e24e4f96e92e885d0e94f0abbfe810442c
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-04-03 07:31:59 -0500

    Prevent recursive pivots in Category.tree method

--- Sources/App/Models/Category+Tree.swift
+++ Sources/App/Models/Category+Tree.swift
@@ -14,9 +14,11 @@ extension Category {
     
     /// Gets the category's sub-categories, and their sub-categories, and so on.
     ///
-    /// - Parameter request: The request that will be used to both run the database queries and create new promises.
+    /// - Parameters:
+    ///   - request: The request that will be used to both run the database queries and create new promises.
+    ///   - fecthed: This parameter is used internaly for the mthod (because it's recursive).
     /// - Returns: A tree structure of categories, made up of nodes.
-    func tree(with request: Request) -> Future<CategoryNode> {
+    func tree(with request: Request, fetched: [Category.ID] = []) -> Future<CategoryNode> {
         
         // Wrap the methods implementation in a `.flatMap` clsure, so the method doesn't have to throw.
         return Future.flatMap(on: request, { () -> EventLoopFuture<CategoryNode> in
@@ -27,7 +29,17 @@ extension Category {
             return children.flatMap(to: [CategoryNode].self) { (children) in
                 
                 // Get all the sub-categories' nodes.
-                return children.map({ $0.tree(with: request) }).flatten(on: request)
+                return try children.map({ (child: Category)throws -> Future<CategoryNode> in
+                    
+                    // Verify the the category has not been fetched in the same branch before.
+                    // If it has, abort. We found a recursive pivot.
+                    let id = try child.requireID()
+                    guard fetched.contains(id) else {
+                        throw Abort(.internalServerError, reason: "Found recursive category/category pivot with id '\(id)'")
+                    }
+                    
+                    return child.tree(with: request, fetched: fetched + [id])
+                }).flatten(on: request)
             }.map(to: CategoryNode.self) { (childNodes) in
                 
                 // Create the top node from the current categories and child nodes.

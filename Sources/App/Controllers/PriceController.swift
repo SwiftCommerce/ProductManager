09b8809ec7eeb4224a3688782da0c47b58524657
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-04-16 15:01:36 -0500

    Created POST /prices PriceController route

--- Sources/App/Controllers/PriceController.swift
+++ Sources/App/Controllers/PriceController.swift
@@ -16,11 +16,20 @@ final class PriceController: RouteCollection {
         // because all routes will start with the same path.
         let prices = router.grouped("prices")
         
+        // Create a POST route at `/prices`
+        prices.post(Price.self, use: create)
+        
         // Create a GET route at `/prices`
         prices.get(use: index)
         
         // Create a GET route at `/prices/:price`
         prices.get(Price.parameter, use: show)
+    }
+    
+    /// Takes a `Price` model decoded from a request
+    /// and saves it to the database
+    func create(_ request: Request, _ price: Price)throws -> Future<Price> {
+        return price.save(on: request)
     }
     
     /// Gets all the `Price` models from the database

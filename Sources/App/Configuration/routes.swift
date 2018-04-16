8ecfbe40721db2ea2a73e62269f0c0b4d0884e7e
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-04-16 15:17:41 -0500

    Register the PriceController route collection with the application's main router

--- Sources/App/Configuration/routes.swift
+++ Sources/App/Configuration/routes.swift
@@ -4,8 +4,9 @@
 public func routes(_ router: Router) throws {
     
     // Register all the controllers with the application's router.
+    try router.register(collection: PriceController())
     try router.register(collection: ProductController())
     try router.register(collection: CategoryController())
-    try router.register(collection: TranslationController())
     try router.register(collection: AttributesController())
+    try router.register(collection: TranslationController())
 }

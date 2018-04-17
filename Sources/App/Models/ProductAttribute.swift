8211a43064ab1df5c20e74aa00a1ea5435ba6028
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-04-17 08:45:37 -0500

    Created ProductAttribute.language property

--- Sources/App/Models/ProductAttribute.swift
+++ Sources/App/Models/ProductAttribute.swift
@@ -10,12 +10,14 @@ final class ProductAttribute: MySQLPivot, Migration {
     var id: Int?
     
     var value: String
+    var language: String
     var productID: Product.ID
     var attributeID: Attribute.ID
     
-    init(value: String, product: Product, attribute: Attribute)throws {
+    init(value: String, language: String, product: Product, attribute: Attribute)throws {
         self.productID = try product.requireID()
         self.attributeID = try attribute.requireID()
         self.value = value
+        self.language = language
     }
 }

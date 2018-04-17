7efdf8af61c5148d2e5ccce2b8943d283ad16c39
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-04-17 15:32:54 -0500

    Created Siblings<Product, Attribute, ProductAttribute>.response(on:pivotQuery:) method

--- Sources/App/Models/ProductAttribute.swift
+++ Sources/App/Models/ProductAttribute.swift
@@ -1,4 +1,6 @@
 import FluentMySQL
+import FluentSQL
+import Vapor
 
 final class ProductAttribute: MySQLPivot, Migration {
     static var leftIDKey: WritableKeyPath<ProductAttribute, Int> = \.productID
@@ -25,5 +27,24 @@ final class ProductAttribute: MySQLPivot, Migration {
 extension Product {
     var attributes: Siblings<Product, Attribute, ProductAttribute> {
         return self.siblings()
+    }
+}
+
+
+extension Siblings where Base == Product, Related == Attribute, Through == ProductAttribute {
+    func response(on request: Request, pivotQuery: QueryBuilder<ProductAttribute, ProductAttribute>? = nil)throws -> Future<[AttributeContent]> {
+        let pivots = try (pivotQuery ?? self.pivots(on: request)).all()
+        let attributes = pivots.flatMap(to: [Attribute].self) { pivots in
+            let ids = pivots.map({ $0.attributeID })
+            return try Attribute.query(on: request).filter(\.id ~~ ids).all()
+        }
+        
+        return Async.map(to: [AttributeContent].self, pivots, attributes) { (pivots, attributes) in
+            let ascendingPivots = pivots.sorted { first, second in return first.attributeID < second.attributeID }
+            let ascendingAttributes = try attributes.sorted { first, second in return try first.requireID() < second.requireID() }
+            return zip(ascendingPivots, ascendingAttributes).map { (response) in
+                return AttributeContent(attribute: response.1, pivot: response.0)
+            }
+        }
     }
 }

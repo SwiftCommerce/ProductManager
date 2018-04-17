43c48b8db52a58d7f81025cbffb79322f7a4e05f
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-04-17 08:44:04 -0500

    Created ProductAttribute.value property

--- Sources/App/Models/ProductAttribute.swift
+++ Sources/App/Models/ProductAttribute.swift
@@ -9,11 +9,13 @@ final class ProductAttribute: MySQLPivot, Migration {
     
     var id: Int?
     
+    var value: String
     var productID: Product.ID
     var attributeID: Attribute.ID
     
-    init(product: Product, attribute: Attribute)throws {
+    init(value: String, product: Product, attribute: Attribute)throws {
         self.productID = try product.requireID()
         self.attributeID = try attribute.requireID()
+        self.value = value
     }
 }

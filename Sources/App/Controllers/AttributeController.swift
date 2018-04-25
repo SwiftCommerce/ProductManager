ac77e2c074b0e1ea159ae3f97c8b71353a2079d0
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-04-25 06:36:56 -0500

    Created AttributeController.create route handler

--- Sources/App/Controllers/AttributeController.swift
+++ Sources/App/Controllers/AttributeController.swift
@@ -3,4 +3,8 @@ import Vapor
 
 final class AttributeController: RouteCollection {
     func boot(router: Router) throws {}
+    
+    func create(_ request: Request, _ attribute: Attribute)throws -> Future<Attribute> {
+        return attribute.save(on: request)
+    }
 }

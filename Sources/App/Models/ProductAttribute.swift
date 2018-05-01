a1939db2ac56299b472a53345a740c0a63823107
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-05-01 08:35:31 -0500

    Documented Siblings<Product, Attribute, ProductAttribute>.response(on:pivotQuery:) method

--- Sources/App/Models/ProductAttribute.swift
+++ Sources/App/Models/ProductAttribute.swift
@@ -2,6 +2,8 @@ import FluentMySQL
 import FluentSQL
 import Vapor
 
+/// Connects a `Product` model to an `Attribute` model
+/// using their IDs.
 final class ProductAttribute: MySQLPivot, ProductModel {
     typealias Left = Product
     typealias Right = Attribute
@@ -37,7 +39,22 @@ extension Product {
 
 
 extension Siblings where Base == Product, Related == Attribute, Through == ProductAttribute {
+    
+    /// Creates a public representation of the `Attribute` models
+    /// connected to a `Product` through `ProductAttribute` pivots.
+    ///
+    /// - Parameters:
+    ///   - request: A request object, which will be used as
+    ///     the worker to run the neccesary database queries.
+    ///   - pivotQuery: The query to use to get the pivots that
+    ///     the `Attribute` models will be fetched with. If `nil`
+    ///     is passed in, we default to getting all the pivots.
+    ///
+    /// - Returns: An array of `AttributeContent`, wrapped in a future.
     func response(on request: Request, pivotQuery: QueryBuilder<ProductAttribute, ProductAttribute>? = nil)throws -> Future<[AttributeContent]> {
+        
+        // Get all the attributes models connected to the pivots
+        // from the pivot query.
         let pivots = try (pivotQuery ?? self.pivots(on: request)).all()
         let attributes = pivots.flatMap(to: [Attribute].self) { pivots in
             let ids = pivots.map({ $0.attributeID })
@@ -45,8 +62,14 @@ extension Siblings where Base == Product, Related == Attribute, Through == Produ
         }
         
         return Async.map(to: [AttributeContent].self, pivots, attributes) { (pivots, attributes) in
+            
+            // We can gurentee that we will have the same amount of pivots as atrributes.
+            // Or organize them so when the sequences are zipped togeather, we will always access
+            // the  pivot that is connected to the attributes,
             let ascendingPivots = pivots.sorted { first, second in return first.attributeID < second.attributeID }
             let ascendingAttributes = try attributes.sorted { first, second in return try first.requireID() < second.requireID() }
+            
+            // Convert each pivot/attribute pair to a `AttributeContent` instance.
             return zip(ascendingPivots, ascendingAttributes).map { (response) in
                 return AttributeContent(attribute: response.1, pivot: response.0)
             }

eb49b36e3d31dae5862039d9c4f27c212fc74a10
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-05-03 08:53:31 -0500

    Added 'any' route element to the root of all paths

--- Sources/App/Configuration/routes.swift
+++ Sources/App/Configuration/routes.swift
@@ -3,11 +3,17 @@
 /// [Learn More →](https://docs.vapor.codes/3.0/getting-started/structure/#routesswift)
 public func routes(_ router: Router) throws {
     
+    // Allows passing in a version path to the route
+    // which is used by ths host's load balancer to
+    // direct to the correct services, while at the
+    // same time allowing us to ignore the value.
+    let versioned = router.grouped(any)
+    
     // Register all the controllers with the application's router.
-    try router.register(collection: PriceController())
-    try router.register(collection: ProductController())
-    try router.register(collection: CategoryController())
-    try router.register(collection: AttributeController())
-    try router.register(collection: TranslationController())
-    try router.register(collection: ProductAttributesController())
+    try versioned.register(collection: PriceController())
+    try versioned.register(collection: ProductController())
+    try versioned.register(collection: CategoryController())
+    try versioned.register(collection: AttributeController())
+    try versioned.register(collection: TranslationController())
+    try versioned.register(collection: ProductAttributesController())
 }

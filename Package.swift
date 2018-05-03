37d6b91e6e19527b92042969f0c42acfc8859782
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-05-03 09:19:30 -0500

    Installed JWTMIddleware package

--- Package.swift
+++ Package.swift
@@ -5,10 +5,11 @@ let package = Package(
     name: "ProductManager",
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0-rc"),
-        .package(url: "https://github.com/vapor/fluent-mysql.git", from: "3.0.0-rc")
+        .package(url: "https://github.com/vapor/fluent-mysql.git", from: "3.0.0-rc"),
+        .package(url: "https://github.com/skelpo/JWTMiddleware.git", from: "0.5.0")
     ],
     targets: [
-        .target(name: "App", dependencies: ["FluentMySQL", "Vapor"]),
+        .target(name: "App", dependencies: ["FluentMySQL", "Vapor", "JWTMiddleware"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"]),
     ]

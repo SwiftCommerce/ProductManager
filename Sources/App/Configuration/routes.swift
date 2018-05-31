951e466a0be86b2cf0f5b950a80f1b1021d847d4
Ralph Küpper <ralph.kuepper@skelpo.com>
2018-05-31 05:44:29 -0400

    Added health route and 'products' slug.

--- Sources/App/Configuration/routes.swift
+++ Sources/App/Configuration/routes.swift
@@ -3,11 +3,15 @@
 /// [Learn More →](https://docs.vapor.codes/3.0/getting-started/structure/#routesswift)
 public func routes(_ router: Router) throws {
     
+    router.get(any, "products", "health") { (request) in
+        return "All Good!"
+    }
+    
     // Allows passing in a version path to the route
     // which is used by ths host's load balancer to
     // direct to the correct services, while at the
     // same time allowing us to ignore the value.
-    let versioned = router.grouped(any).grouped(ModificationProtectionMiddleware())
+    let versioned = router.grouped(any, "products").grouped(ModificationProtectionMiddleware())
     
     // Register all the controllers with the application's router.
     try versioned.register(collection: PriceController())

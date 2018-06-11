a71215d041eb5d089185cc1e0ec4fab01eb30148
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-06-11 15:47:28 -0500

    Installed MySQLFluentQuery package

--- Package.swift
+++ Package.swift
@@ -6,10 +6,11 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
         .package(url: "https://github.com/vapor/fluent-mysql.git", from: "3.0.0-rc"),
-        .package(url: "https://github.com/skelpo/JWTMiddleware.git", from: "0.6.0")
+        .package(url: "https://github.com/skelpo/JWTMiddleware.git", from: "0.6.0"),
+        .package(url: "https://github.com/skelpo/FluentQuery.git", .branch("mysql"))
     ],
     targets: [
-        .target(name: "App", dependencies: ["FluentMySQL", "Vapor", "JWTMiddleware"]),
+        .target(name: "App", dependencies: ["FluentMySQL", "Vapor", "JWTMiddleware", "MySQLFluentQuery"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"]),
     ]

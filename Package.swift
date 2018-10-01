b9b9ebf3a85e49ff941f41c992493448a8a9521c
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-10-01 06:09:32 -0500

    Removed MySQLFluentQuery dependency from 'App' target

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         .package(url: "https://github.com/skelpo/JWTMiddleware.git", from: "0.6.0")
     ],
     targets: [
-        .target(name: "App", dependencies: ["FluentMySQL", "Vapor", "JWTMiddleware", "MySQLFluentQuery"]),
+        .target(name: "App", dependencies: ["FluentMySQL", "Vapor", "JWTMiddleware"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"]),
     ]

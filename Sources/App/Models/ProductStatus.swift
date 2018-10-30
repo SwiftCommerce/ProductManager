1722267eb8fee63d54750eb25283e4fe2b609321
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-10-30 14:47:46 -0500

    Removed commented-out ProductStatus code

--- Sources/App/Models/ProductStatus.swift
+++ Sources/App/Models/ProductStatus.swift
@@ -12,38 +12,3 @@ enum ProductStatus: Int, Codable {
     /// The product was available for viewing, but currently it is not.
     case deactivated
 }
-
-//extension ProductStatus: URLEncodedFormDataConvertible {
-//    
-//}
-/*
-extension ProductStatus: MySQLColumnDefinitionStaticRepresentable {
-    
-    /// The type a column should be in the database when a model
-    /// has a property of type `ProductStatus`.
-    static var mySQLColumnDefinition: MySQLColumnDefinition = .smallInt()
-}
-
-extension ProductStatus: MySQLDataConvertible {
-    
-    /// Creates a `MySQLData` representation of the current case.
-    func convertToMySQLData() throws -> MySQLData {
-        return .init(integer: self.rawValue)
-    }
-    
-    /// Creates `ProductStatus` case from a `MySQLData` object.
-    static func convertFromMySQLData(_ mysqlData: MySQLData) throws -> ProductStatus {
-        
-        // Get the raw value as an `Int`. If the value is enot an int, throw.
-        guard let rawValue = try mysqlData.integer(Int.self) else {
-            throw MySQLError(identifier: "badType", reason: "A `ProdcutStatus` case must be initialized with an integer", possibleCauses: .capture())
-        }
-        
-        // Create a status from the raw value. If the value is invalid, throw.
-        guard let status = ProductStatus(rawValue: rawValue) else {
-            throw MySQLError(identifier: "badInt", reason: "No case exists for`ProductStatus` enum with the raw value \(rawValue)", possibleCauses: .capture())
-        }
-        
-        return status
-    }
-}*/
